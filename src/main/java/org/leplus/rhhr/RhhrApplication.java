4173b4df806ac41d9f66fcfa1870be6e007ffcc6
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-11-11 18:54:51 -0800

    Adding Spring Initializr project

---
+++ src/main/java/org/leplus/rhhr/RhhrApplication.java
@@ -0,0 +1,13 @@
+package org.leplus.rhhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhhrApplication.class, args);
+	}
+
+}
