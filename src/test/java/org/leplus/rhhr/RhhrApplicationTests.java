0ae80f436862d6f58b4250354d296679e1148c88
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-11-11 21:47:01 -0800

    Adding test for redirection

--- src/test/java/org/leplus/rhhr/RhhrApplicationTests.java
+++ src/test/java/org/leplus/rhhr/RhhrApplicationTests.java
@@ -1,13 +1,44 @@
 package org.leplus.rhhr;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
-class RhhrApplicationTests {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class RhhrApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        final RestTemplate restTemplate = new RestTemplate();
+        final HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        final CloseableHttpClient build = HttpClientBuilder.create()
+                .disableRedirectHandling().build();
+        factory.setHttpClient(build);
+        restTemplate.setRequestFactory(factory);
+        return restTemplate;
+    }
+
+    @Test
+    public void testRedirection() throws Exception {
+        assertEquals("/login", restTemplate
+                .getForEntity("http://localhost:" + port + "/", String.class)
+                .getHeaders().getLocation().toString());
+    }
 
 }
